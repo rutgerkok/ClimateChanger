56d37141b0039a9b0fea3f851c5835efb89f8d3e
Rutger Kok <rutger.kok@live.com>
2021-06-06 20:48:15 +0200

    Support the new biome arrays
    They are now 3d int, instead of 2d byte arrays.

--- src/main/java/nl/rutgerkok/climatechanger/task/BiomeIdChanger.java
+++ src/main/java/nl/rutgerkok/climatechanger/task/BiomeIdChanger.java
@@ -4,8 +4,8 @@ import nl.rutgerkok.hammer.anvil.AnvilChunk;
 import nl.rutgerkok.hammer.util.Result;
 
 public class BiomeIdChanger implements ChunkTask {
-    private final byte from;
-    private final byte to;
+    private final int from;
+    private final int to;
 
     /**
      * Changes the biome id in all files in the given directory.
@@ -16,14 +16,18 @@ public class BiomeIdChanger implements ChunkTask {
      *            The id to convert to, use -1 to let Minecraft recalculate the
      *            biomes when it loads the world.
      */
-    public BiomeIdChanger(byte from, byte to) {
+    public BiomeIdChanger(int from, int to) {
         this.from = from;
         this.to = to;
     }
 
     @Override
     public Result convertChunk(AnvilChunk chunk) {
-        return handleByteArray(chunk.getBiomeArray()) ? Result.CHANGED : Result.NO_CHANGES;
+        int[] biomeArray = chunk.getBiomeArray();
+        if (biomeArray.length == 0) {
+            return handleByteArray(chunk.getOldBiomeArray()) ? Result.CHANGED : Result.NO_CHANGES;
+        }
+        return handleIntArray(biomeArray) ? Result.CHANGED : Result.NO_CHANGES;
     }
 
     @Override
@@ -40,6 +44,28 @@ public class BiomeIdChanger implements ChunkTask {
      * @return Whether changes were made to the array.
      */
     protected boolean handleByteArray(byte[] biomeIds) {
+        if (((byte) to) != to) {
+            return false; // Cannot store this biome
+        }
+        boolean hasChanges = false;
+        for (int i = 0; i < biomeIds.length; i++) {
+            if (from == -1 || biomeIds[i] == from) {
+                biomeIds[i] = (byte) to;
+                hasChanges = true;
+            }
+        }
+        return hasChanges;
+    }
+
+    /**
+     * Converts all ids in the byte array. The original byte array will be
+     * modified.
+     *
+     * @param biomeIds
+     *            The bytes to convert.
+     * @return Whether changes were made to the array.
+     */
+    private boolean handleIntArray(int[] biomeIds) {
         boolean hasChanges = false;
         for (int i = 0; i < biomeIds.length; i++) {
             if (from == -1 || biomeIds[i] == from) {
