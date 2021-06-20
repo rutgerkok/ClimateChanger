32834b84716a95021ec5f97cc7f2a02ba03845a5
Rutger Kok <rutger.kok@live.com>
2021-06-20 15:50:20 +0200

    Round progress to one digit after comma

--- src/main/java/nl/rutgerkok/climatechanger/gui/ProgressPanel.java
+++ src/main/java/nl/rutgerkok/climatechanger/gui/ProgressPanel.java
@@ -1,14 +1,14 @@
 package nl.rutgerkok.climatechanger.gui;
 
-import nl.rutgerkok.climatechanger.ProgressUpdater;
-import nl.rutgerkok.hammer.util.Progress;
-
 import java.awt.Color;
 import java.util.Objects;
 
 import javax.swing.JLabel;
 import javax.swing.JProgressBar;
 import javax.swing.SwingUtilities;
+
+import nl.rutgerkok.climatechanger.ProgressUpdater;
+import nl.rutgerkok.hammer.util.Progress;
 
 public class ProgressPanel implements ProgressUpdater {
     private final Color originalBarForeground;
@@ -41,20 +41,6 @@ public class ProgressPanel implements ProgressUpdater {
     }
 
     @Override
-    public void update(final Progress progress) {
-        // It's one tenth second since the last progress update, update bar
-        // Make sure bar is updated on correct thread
-        SwingUtilities.invokeLater(new Runnable() {
-
-            @Override
-            public void run() {
-                progressBar.setValue(progress.getIntPercentage());
-                progressField.setText(progress.getIntPercentage() + "%");
-            }
-        });
-    }
-
-    @Override
     public void init() {
         progressBar.setValue(0);
         progressBar.setMaximum(100);
@@ -63,6 +49,25 @@ public class ProgressPanel implements ProgressUpdater {
         // Reset foreground, in case previous attempt failed
         progressBar.setForeground(originalBarForeground);
         progressBar.setStringPainted(false);
+    }
+
+    private double roundToSingleDigit(double number) {
+        int intNumber = (int) (number * 10);
+        return intNumber / 10.0;
+    }
+
+    @Override
+    public void update(final Progress progress) {
+        // It's one tenth second since the last progress update, update bar
+        // Make sure bar is updated on correct thread
+        SwingUtilities.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+                progressBar.setValue(progress.getIntPercentage());
+                progressField.setText(roundToSingleDigit(progress.getPercentage()) + "%");
+            }
+        });
     }
 
 }
